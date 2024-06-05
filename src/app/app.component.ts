41510e03953bf82256e326521d721c3a05a70fb2
jpca92 <jpca92@gmail.com>
2024-06-05 11:12:35 -0500

    primera clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  welcome = 'Hola';
+  tasks = [
+    'Instalar angular cli',
+    'crear proyecto',
+    'crear componentes'
+  ]
 }
