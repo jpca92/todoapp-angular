c4ab6becf9a971b24ae24463b207b23812b5635b
jpca92 <jpca92@gmail.com>
2024-06-05 11:30:22 -0500

    clase 2

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+
+  welcome = 'Hola';
+  tasks = [
+    'Instalar angular cli',
+    'crear proyecto',
+    'crear componentes'
+  ]
+
+}
